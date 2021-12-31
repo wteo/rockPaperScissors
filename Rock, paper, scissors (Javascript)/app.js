36e291672ce1df365722eb30e7a022d3a9e08d24
wteo <87306585+wteo@users.noreply.github.com>
2021-12-31 12:47:57 +1100

    Update app.js

--- Rock, paper, scissors (Javascript)/app.js
+++ Rock, paper, scissors (Javascript)/app.js
@@ -5,6 +5,7 @@ const figure = document.querySelector("figure");
 const images = document.querySelectorAll(".image");
 
 
+
 function border() {
     const images = document.querySelectorAll(".image");
     for (let image of images) {
@@ -17,26 +18,65 @@ function border() {
         }
     }
 
+
+
 function random() {
     const choices = [rock, paper, scissors];
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
     return computerChoice;
     }
 
+
+
 for (let playerTurn of images) {
+
     playerTurn.addEventListener("click", function() {
+
         const computerTurn = random();
+        
         if (playerTurn === computerTurn) {
+            
             figure.innerHTML = "It's a tie!";
+
+            const playerResult = document.createElement("p");
+            playerResult.innerHTML = `You chose: ${playerTurn}`;
+            figure.appendChild(playerResult);
+            
+            const computerResult = document.createElement("p");
+            computerResult.innerHTML = `Your computer chose: ${computerTurn}`;
+            figure.appendChild(computerResult);
+           
             console.log(playerTurn);
             console.log(computerTurn);
+
             } else if (playerTurn === rock && computerTurn === scissors || playerTurn === paper && computerTurn === rock ||
             playerTurn === scissors && computerTurn === paper) {
+
             figure.innerHTML = "You win!";
+
+            const playerResult = document.createElement("p");
+            playerResult.innerHTML = `You chose: ${playerTurn}`;
+            figure.appendChild(playerResult);
+            
+            const computerResult = document.createElement("p");
+            computerResult.innerHTML = `Your computer chose: ${computerTurn}`;
+            figure.appendChild(computerResult);
+
             console.log(playerTurn);
             console.log(computerTurn);
+
             } else {
+            
             figure.innerHTML = "Sorry, you lose. :-( Please try again.";
+            
+            const playerResult = document.createElement("p");
+            playerResult.innerHTML = `You chose: ${playerTurn}`;
+            figure.appendChild(playerResult);
+            
+            const computerResult = document.createElement("p");
+            computerResult.innerHTML = `Your computer chose: ${computerTurn}`;
+            figure.appendChild(computerResult);
+
             console.log(playerTurn);
             console.log(computerTurn); 
             }
