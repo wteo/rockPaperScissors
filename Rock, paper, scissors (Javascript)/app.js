c9e13f06920041e6a573e5349870a38b959c2730
wteo <87306585+wteo@users.noreply.github.com>
2022-01-02 09:57:58 +1100

    Update app.js

--- Rock, paper, scissors (Javascript)/app.js
+++ Rock, paper, scissors (Javascript)/app.js
@@ -8,14 +8,14 @@ function random() {
     const choices = [rock, paper, scissors];
     const randomization = choices[Math.floor(Math.random() * choices.length)];
     return randomization;
-    }
+}
 
 function resize() {
     for (let image of images) {
         image.style.width = "100px";
         image.style.height = "100px";
-        }
     }
+}
 
 function getResults(chosenImage, statement) {
     const p = document.createElement("p");
@@ -24,30 +24,40 @@ function getResults(chosenImage, statement) {
     figure.append(p);
     p.append(br);
     p.append(chosenImage);
+}
+
+function handlePlayerClick(event) {
+    const computer = random();
+    const player = event.currentTarget;
+    if (player === computer) {
+        figure.innerText = "It's a tie!";
+        getResults(player, "Both you and your computer chose:");
+        resize();
+    } else if (player === rock && computer === scissors || player === paper && computer === rock ||
+        player === scissors && computer === paper) {
+        figure.innerText = "You win!";
+        getResults(player, "You chose:");
+        getResults(computer, "Your computer chose:");
+        resize();
+    } else {
+        figure.innerText = "Sorry, you lose. :-( Please try again.";
+        getResults(player, "You chose:");
+        getResults(computer, "Your computer chose:");
+        resize();
     }
+    gameOver();
+}
 
 function playerInput() {
-    for (let player of images) {
-        player.addEventListener("click", function() {
-            const computer = random();
-            if (player === computer) {
-                    figure.innerText = "It's a tie!";
-                    getResults(player, "Both you and your computer chose:");
-                    resize();
-               } else if (player === rock && computer === scissors || player === paper && computer === rock ||
-                player === scissors && computer === paper) {
-                    figure.innerText = "You win!";
-                    getResults(player, "You chose:");
-                    getResults(computer, "Your computer chose:");
-                    resize();
-                } else {
-                figure.innerText = "Sorry, you lose. :-( Please try again.";
-                getResults(player, "You chose:");
-                getResults(computer, "Your computer chose:");
-                resize();
-                }
-            })
-        }
+    for (let image of images) {
+        image.addEventListener("click", handlePlayerClick)
     }
+}
+
+function gameOver() {
+    for (let image of images) {
+        image.removeEventListener("click", handlePlayerClick)
+    }
+}
 
 playerInput();
